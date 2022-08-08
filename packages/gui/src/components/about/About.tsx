58c0b504f28039de4e2f1dac05cb1687a76a4b8e
zcomputerwiz <zcomputerwiz@users.noreply.github.com>
2022-08-08 10:24:19 -0500

    Match Maize Icons and Logos

--- packages/gui/src/components/about/About.tsx
+++ packages/gui/src/components/about/About.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled, { createGlobalStyle } from 'styled-components';
-import icon from '../../assets/img/chia_circle.svg';
+import icon from '../../assets/img/maize_circle.svg';
 
 const GlobalStyle = createGlobalStyle`
   body,
